17e51c4096b1f64a3a4ab787363148dc7a089370
Ares <89419093+arestosora@users.noreply.github.com>
2024-08-04 14:51:12 -0500

    chore: removed unused controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './modules/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AuthService } from './modules/auth/services/auth.service';
@@ -9,7 +7,7 @@ import { SessionPassportModule } from './modules/passport/passport.module';
 
 @Module({
   imports: [DatabaseModule, AuthModule, UsersModule, SessionPassportModule],
-  controllers: [AppController],
-  providers: [AppService, AuthService],
+  controllers: [],
+  providers: [AuthService],
 })
 export class AppModule { }
--- tests/auth.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AuthGuard } from '../src/modules/auth/guards/auth.guard';
-
-describe('AuthGuard', () => {
-  it('should be defined', () => {
-    expect(new AuthGuard()).toBeDefined();
-  });
-});
